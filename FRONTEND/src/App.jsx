5d5c95d33671baa13df842cea58b28eb55f7b54f
Leto01 <80838862+Leto01@users.noreply.github.com>
2023-06-08 11:23:04 +0200

    init frontend: vite with React (JS)

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/" index element={<LandingPage></LandingPage>}/>
+    </Routes>
+  </Router>
+}
+
+export default App;
---
+++ FRONTEND/src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+const LandingPage = ()=>{
+    return(<>
+        <div className="pageContent">
+            
+        </div>
+    </>);
+}
+
+export default LandingPage;
