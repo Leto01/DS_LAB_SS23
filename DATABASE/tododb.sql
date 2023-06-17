af2f17c60e38dc86f532df6c6244227fe8083ec3
leto.ziegler <leto.ziegler@quanto-solutions.de>
2023-06-17 21:53:55 +0200

    Backend runs after building all dokcer-files with docker compose

---
+++ DATABASE/tododb.sql
@@ -0,0 +1,13 @@
+CREATE TABLE myservice_task (
+    id character varying(36),
+    todo character varying(500) NULL,
+    priority integer NULL
+);
+
+CREATE TABLE distributedsystems_models_task (
+    id character varying(36),
+    todo character varying(500) NULL,
+    priority integer NULL
+);
+
+CREATE VIEW myservice_task AS SELECT * FROM myservice_task; 
